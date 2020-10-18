5130ebddb25f2dd4674b6fd28169b39fd6a2c060
Slava <6891094@mail.ru>
2020-10-18 14:53:19 +0300

    changed file task_10_08.py

--- hw_10_11/task_10_08.py
+++ hw_10_11/task_10_08.py
@@ -1,12 +1,12 @@
 import csv
-from csv_utils import read_file, adding_text, del_text
+from csv_utils import read_file, adding_text, del_text, sum_price, max_price
 
 fields = ['Name of product', 'price', 'number', 'comments']
 rows = [
     ['Tea', 2.0, 40, 'Nice tea'],
-    ['Coffee', 7.5, 23, 'Good coffee'],
-    ['Sweets', 6.8, 32, 'very sweet'],
-    ['Cookie', 3.3, 100, 'tasteless'],
+    ['Coffee', 7.3, 23, 'Good coffee'],
+    ['Sweets', 6.2, 32, 'very sweet'],
+    ['Cookie', 3.1, 100, 'tasteless'],
 ]
 filename = 'students_info.csv'
 with open(filename, 'w') as csvfile:
@@ -14,7 +14,7 @@ with open(filename, 'w') as csvfile:
     csvwrite.writerow(fields)
     csvwrite.writerows(rows)
 
-
 # adding_text('students_info.csv')
 # del_text('students_info.csv', 3)
 # print(read_file('students_info.csv'))
+# sum_price('students_info.csv', rows)
