515dca09fa81b7f36fd68406865d19176eb047e3
Slava <6891094@mail.ru>
2020-10-22 00:13:13 +0300

    changed task_11_2.py

--- HW11_OOP/task_11_2.py
+++ HW11_OOP/task_11_2.py
@@ -1,15 +1,17 @@
 class Car:
-    def __init__(self, brand, model, year_of_car_manufacture, speed=0):
+    def __init__(self, brand, model, year_of_car_manufacture, speed: int = 0):
         self.__brand = brand
         self.__model = model
         self.__year_of_car_manufacture = year_of_car_manufacture
         self.__speed = speed
 
-    def up_speed(self):
-        return self.__speed + 5
+    def up_speed(self, km: int = 5):
+        self.__speed += km
+        return self.__speed
 
-    def slow_down_speed(self):
-        return self.__speed - 5
+    def slow_down_speed(self, km: int = 5):
+        self.__speed -= km
+        return self.__speed
 
     def get_stop_speed(self):
         return self.__speed
@@ -17,8 +19,15 @@ class Car:
     def set_stop_speed(self):
         self.__speed = 0
 
+    def upend_speed(self):
+        self.__speed = -self.__speed
+        return self.__speed
+
     def show_speed(self):
         print(self.__speed)
 
 
 car = Car(brand='Bentley', model='continental', year_of_car_manufacture=2016)
+
+
+
